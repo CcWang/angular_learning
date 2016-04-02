5b746ad0d34032e9f3c3434b984ebd80b4182d2d
yixizhang <chongchong1110@gmail.com>
2016-04-01 18:34:35 -0700

    setting up partials

---
+++ customizingUsers/client/static/script.js
@@ -0,0 +1,29 @@
+var myApp=angular.module('myApp',['ngRoute']);
+myApp.config(function ($routeProvider) {
+	// body...
+	$routeProvider
+		.when('/list', {
+			templateUrl:'partials/list.html'
+		})
+		.when('/users', {
+			templateUrl:'partials/users.html'
+		})
+		.otherwise({
+			redirectTo:'/'
+		});
+})
+myApp.factory('userFactory', function(){
+	var factory={};
+
+	factory.users = [
+		{first_name: "Yukihiro", last_name:"Matsumoto", lan:"Ruby"},
+ 		{first_name:"Ryan", last_name:"Dahl", lan:"JavaScript"},
+ 		{first_name:"Brendan", last_name:"Eich",lan:"JavaScript"}
+	];
+
+	return factory;
+})
+myApp.controller('listController', function($scope){
+	$scope.message = 'We are using another controller';
+   
+})
