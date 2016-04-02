47fed27d08e4f5a67e32c9ea3b4996ccab8dc6a4
Chongchong Wang <chongchong1110@gmail.com>
2016-04-01 19:04:48 -0700

    finish users.html

--- customizingUsers/client/static/script.js
+++ customizingUsers/client/static/script.js
@@ -21,6 +21,14 @@ myApp.factory('userFactory', function(){
  		{first_name:"Brendan", last_name:"Eich",lan:"JavaScript"}
 	];
 
+	factory.removeUser = function(i){
+		factory.users.splice(i,1);
+	};
+
+	factory.addUser = function(data){
+		factory.users.push(data);
+	}
+
 	return factory;
 })
 myApp.controller('listController', function($scope, userFactory){
@@ -29,4 +37,19 @@ myApp.controller('listController', function($scope, userFactory){
    		$scope.users = data;
    	};
    	$scope.getUsers(userFactory.users);
+})
+
+myApp.controller('userController', function($scope,userFactory){
+	$scope.users;
+   	$scope.getUsers = function(data){
+   		$scope.users = data;
+   	};
+   	$scope.getUsers(userFactory.users);
+   	$scope.removeUser = function($index){
+   		userFactory.removeUser($index);
+   	};
+
+   	$scope.addUser = function(){
+   		userFactory.addUser($scope.user);
+   	}
 })
